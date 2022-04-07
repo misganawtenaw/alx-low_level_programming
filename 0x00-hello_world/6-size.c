5e2959b97f8a1743a0d29b5c3e3fba671e4b02c3
misganawtenaw <misganawtenaw@gmail.com>
2022-04-07 11:03:22 -0700

    c task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,5 +12,5 @@ printf("size of an int: %d byte(s)\n", sizeof(int));
 printf("size of a long int: %d byte(s)\n", sizeof(long int));
 printf("size of a long long int: %d byte(s)\n", sizeof(long long int));
 printf("size of float: %d byte(s)\n", sizeof(float));
-return (0)
+return (0);
 }
