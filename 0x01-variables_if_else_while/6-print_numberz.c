7698c5c77e06eb6ce0e1cdb82330e2c3ded334ee
misganawtenaw <misganawtenaw@gmail.com>
2022-04-11 03:10:28 -0700

    c_01 task_6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Descripition: Print numbers 0 to 9 on one line
+ * Return: 0
+ */
+int main(void)
+{
+int i;
+for (i = 0; i <= 9; ++i)
+putchar('0' + i);
+putcha('\n');
+
+return (0);
+}
