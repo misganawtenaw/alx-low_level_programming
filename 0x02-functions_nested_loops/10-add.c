2d634adf637133bb7c8b22187259eba96578d112
misganawtenaw <misganawtenaw@gmail.com>
2022-04-12 18:10:16 -0700

    nested loops task_10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - adds the integers
+ * @num1: the first integer to be added
+ * @num2: the second integer to be added
+ * Return: the result of the addition
+ */
+int _add(int num1, int num2)
+{
+return (num1 + num2);
+}
