5d7357b135af56fb719713f25afdaadfdec19785
misganawtenaw <misganawtenaw@gmail.com>
2022-04-12 17:43:35 -0700

    nested loops task_7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,6 +9,6 @@ int print_last_digit(int n)
 int last_digit = n % 10;
 if (last_digit < 0)
 last_digit *= -1;
-_putchar(last_digit + '0');
-retrun (last_digit);
+_putchar(last_digit + '0'); 
+return (last_digit);
 }
