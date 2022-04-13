4bb2b9557b46cedbd335ee7aeeb5cd96861a66bf
misganawtenaw <misganawtenaw@gmail.com>
2022-04-13 02:04:58 -0700

    nested loops task_15

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Retrun: 0
+ */
+int main(void)
+{
+unsigned long f1 = 0, f2 = 1, fsum;
+float total_sum;
+
+while (1)
+{
+fsum = f1 + f2;
+if (fsum > 4000000)
+break;
+if ((fsum % 2) == 0)
+total_sum += fsum;
+f1 = f2;
+f2 = fsum;
+}
+printf("%ef\n", total_sum);
+return (0);
+}
