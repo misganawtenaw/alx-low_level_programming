13fe8184becbcb2833b89ffe299aa74079f752c8
misganawtenaw <misganawtenaw@gmail.com>
2022-04-13 02:09:26 -0700

    nested loops task_15

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 /**
- * main - entry point
+ * main - prints the sum of even-valued fibonacci
+ *         sequance terms not exceeding 4000000.
+ * 
  * Retrun: 0
  */
 int main(void)
@@ -13,11 +15,14 @@ while (1)
 fsum = f1 + f2;
 if (fsum > 4000000)
 break;
+
 if ((fsum % 2) == 0)
 total_sum += fsum;
+
 f1 = f2;
 f2 = fsum;
 }
 printf("%ef\n", total_sum);
+
 return (0);
 }
