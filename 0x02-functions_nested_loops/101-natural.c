97b2de0f0a568f1d3e73926ef8df3ce28cbeb41a
misganawtenaw <misganawtenaw@gmail.com>
2022-04-13 01:46:34 -0700

    nested loops task_13

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+int x, sum = 0;
+
+for (x = 0; x < 1024; x++)
+{
+if ((x % 3) == 0 || (x % 5) == 0)
+sum += x;
+}
+printf("%d\n", sum);
+return (0);
+}
