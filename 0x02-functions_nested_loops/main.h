ad0e9a97e2f3aa4cbe5d4527771ad488484a891d
misganawtenaw <misganawtenaw@gmail.com>
2022-04-12 18:34:55 -0700

    main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -12,6 +12,6 @@ int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
 int add(int, int);
-void prinit_to_98(int n);
-void prinit_times_table(int n);
+void print_to_98(int n);
+void print_times_table(int n);
 #endif /* _MAIN_H_ */
