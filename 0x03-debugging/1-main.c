3e13831e967f5aa7500c98f21facec2692380dec
misganawtenaw <misganawtenaw@gmail.com>
2022-04-13 23:51:10 -0700

    task_1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -13,15 +13,15 @@ printf("Infinite loop incoming :(\n");
 
 i = 0;
 
-/**
+/*
  * while (i < 10)
  * {
  * infinte loop - No increase of variable
  * putchar(i);
- * }
+}
  */
 
-	printf("Infinite loop avoided! \\o/\n");
+printf("Infinite loop avoided! \\o/\n");
 
-	return (0);
+return (0);
 }
