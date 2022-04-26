fb4b84e1ee11b77ad8b3ac065d304fe60daee7cc
misganawtenaw <misganawtenaw@gmail.com>
2022-04-26 10:15:42 -0700

    task_0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,23 +1,17 @@
 #include "main.h"
-
 /**
- * _memset - function that fills memory with a constant byte.
- *
- * @s: mamory area to return
- * @b: constant byte
- * @n: size of bytes
- * Return: char
+ * _memset - fills memory with a constant byte,
+ * @s: memory area.
+ * @b: constant byte.
+ * @n: bytes filled.
+ * Return: the pointer to dest.
  */
-
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i = 0;
+	unsigned int i;
 
-	while (i < n)
-	{
+	for (i = 0; i < n; i++)
+		*(s + i) =  b;
 
-		*(s + i) = b;
-		i += 1;
-	}
 	return (s);
 }
