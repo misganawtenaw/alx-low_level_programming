201aec170870de6fdd5c6fcaa62b15cd36aa45cd
misganawtenaw <misganawtenaw@gmail.com>
2022-05-23 23:59:58 -0700

    0x13-more_singly_linked_lists

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * listint_len - Prints all the elements
+ * @h: Pointer to an int constant
+ *
+ * Return: The number of mine
+**/
+size_t listint_len(const listint_t *h)
+{
+	unsigned int m = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		m++;
+	}
+	return (m);
+}
