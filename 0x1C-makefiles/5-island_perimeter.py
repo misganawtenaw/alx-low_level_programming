4707d2cf2775cd412da916d7841867dd756a2b5b
misganawtenaw <misganawtenaw@gmail.com>
2022-09-14 02:48:21 -0700

    makefile

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,26 +1,26 @@
 #!/usr/bin/python3
-'''Technical Interview'''
+"""Defines an island perimeter measuring function."""
 
 
 def island_perimeter(grid):
-    '''grid is a list of list of integers:
-        0 represents a water zone
-        1 represents a land zone
-        One cell is a square with side length 1
-        Grid cells are connected horizontally/vertically (not diagonally).
-        Grid is rectangular, width and height don’t exceed 100
-    Grid is completely surrounded by water, and there is
-    one island (or nothing).
-    The island doesn’t have “lakes” (water inside that isn’t connected
-    to the water around the island).'''
-    perimeter = 0
-    grid_length = len(grid)
-    for i in range(grid_length):
-        subgrid = grid[i]
-        subgrid_length = len(subgrid)
-        for j in range(subgrid_length):
+    """Return the perimiter of an island.
+    The grid represents water by 0 and land by 1.
+    Args:
+        grid (list): A list of list of integers representing an island.
+    Returns:
+        The perimeter of the island defined in grid.
+    """
+    width = len(grid[0])
+    height = len(grid)
+    edges = 0
+    size = 0
+
+    for i in range(height):
+        for j in range(width):
             if grid[i][j] == 1:
-                perimeter += 1
-        perimeter *= 2
-        perimeter += 2
-    return perimeter
+                size += 1
+                if (j > 0 and grid[i][j - 1] == 1):
+                    edges += 1
+                if (i > 0 and grid[i - 1][j] == 1):
+                    edges += 1
+    return size * 4 - edges * 2
